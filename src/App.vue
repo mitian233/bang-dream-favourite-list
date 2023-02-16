c4310535323123b5647a415e38c5697283232f05
mitian233 <2304254165@qq.com>
2023-02-16 13:43:19 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import SongsListView from "./components/songsListView.vue";
+</script>
+
+<template>
+  <songs-list-view/>
+  <div style="margin: 10px 0 0 0;padding: 0;">
+    <p>Made with ❤ by Mikan | <a href="https://github.com/mitian233/bang-dream-favourite-list">GitHub</a> | <a href="https://pd.qq.com/s/5smpnsao5">来频道坐坐</a></p>
+  </div>
+</template>
+
+<style scoped>
+/*
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}*/
+</style>
---
+++ src/components/songsListView.vue
@@ -0,0 +1,164 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+const dialogVisible = ref(false)
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('确定要关闭吗？')
+      .then(() => {
+        done()
+      })
+      .catch(() => {
+        // catch error
+      })
+}
+</script>
+<template>
+  <div id="gird">
+    <h1 style="margin-bottom: 30px">邦邦生涯个人喜好表</h1>
+    <div class="grid grid-cols-6 gap-4 w-fit">
+      <div v-for="item in listItem" class="border-4 border-black rounded-xl bg-white h-40 w-40 relative">
+        <div class="bg-white h-37 w-37">
+          <div style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;display: flex;justify-content: center;align-items: center">
+            <div v-if="!item.imgExist">
+              <p>{{item.musicTitle}}</p>
+            </div>
+            <div v-else>
+              <img :src="item.img" alt="">
+            </div>
+          </div>
+        </div>
+        <p class="absolute bottom-1.5 left-2">{{ item.title }}</p>
+      </div>
+    </div>
+  </div>
+  <el-dialog v-model="pickUpVisible" title="选择歌曲" width="30%" :before-close="handleClose" draggable>
+    <span>选择单元格：</span>
+    <el-select v-model="selectBoxIndex" class="m-2" placeholder="Select" size="large">
+      <el-option v-for="item in listItem" :key="item.title" :label="item.title" :value="listItem.indexOf(item)"/>
+    </el-select>
+    <br/>
+    <span>　选择乐曲：</span>
+    <el-select v-model="selectValue" class="m-2" placeholder="Select" size="large">
+      <el-option v-for="item in songsList" :key="item" :label="item" :value="songsList.indexOf(item)"/>
+    </el-select>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="pickUpVisible = false">取消</el-button>
+        <el-button type="danger" @click="deleteAll">清除所有</el-button>
+        <el-button type="danger" @click="deleteItem(selectBoxIndex)">清除选中</el-button>
+        <el-button type="primary" @click="editedAndSave(selectBoxIndex,selectValue)">
+          保存
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <div style="margin-top: 10px">
+    <el-button @click="saveImg">保存成图片</el-button>
+    <el-button @click="editWindow">编辑</el-button>
+  </div>
+  <el-dialog v-model="saveImgDialogVisible" title="生成记录" width="70%">
+    <div id="saveimagecanvas"></div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="danger" @click="deleteRecord">删除生成记录</el-button>
+        <el-button type="primary" @click="saveImgDialogVisible = false">关闭</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import html2canvas from "html2canvas";
+import axios from "axios";
+import songsListRaw from '../assets/all.5.js'
+export default {
+  name: "songsListView",
+  data() {
+    return {
+      listItem: [
+        {imgExist: false, img: '',musicTitle:'', title: '最爱的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最惊艳的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最长情的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最有感触的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最有意义的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最讨厌的'},
+        {imgExist: false, img: '',musicTitle:'', title: '效率最高的'},
+        {imgExist: false, img: '',musicTitle:'', title: '影响我最深的'},
+        {imgExist: false, img: '',musicTitle:'', title: '带新人入坑必推的'},
+        {imgExist: false, img: '',musicTitle:'', title: '入坑玩的第一首'},
+        {imgExist: false, img: '',musicTitle:'', title: '打的最后一首'},
+        {imgExist: false, img: '',musicTitle:'', title: '协力必点的'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的歌词'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的编曲'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的吉他'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的贝斯'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的鼓点'},
+        {imgExist: false, img: '',musicTitle:'', title: '最喜欢的键盘/弦乐'},
+      ],
+      songsList: [],
+      selectBoxIndex: 0,
+      selectValue: 0,
+      saveImgDialogVisible: false,
+      pickUpVisible: false,
+    }
+  },
+  methods: {
+    addImg(item, girdIndex) {
+      console.log('addImg', item, girdIndex)
+    },
+    saveImg() {
+      this.saveImgDialogVisible = true
+      console.log('saveImg')
+      html2canvas(document.querySelector("#gird")).then(canvas => {
+        document.querySelector('#saveimagecanvas').appendChild(canvas).style.width = '100%'
+      })
+    },
+    editWindow() {
+      this.pickUpVisible = true
+    },
+    deleteItem(target) {
+      this.listItem[target].musicTitle = ''
+    },
+    deleteAll() {
+      for (let i = 0; i < this.listItem.length; i++) {
+        this.listItem[i].musicTitle = ''
+      }
+    },
+    deleteRecord() {
+      const canvasDiv = document.getElementById("saveimagecanvas");
+      while (canvasDiv.firstChild) {
+        canvasDiv.removeChild(canvasDiv.firstChild);
+      }
+    },
+    editedAndSave(target,value) {
+      console.log(this.listItem[target].title,'是',this.songsList[value])
+      this.listItem[target].musicTitle = this.songsList[value]
+    }
+  },
+  mounted() {
+    const _this = this
+    songsListRaw["146"]
+    for (let i = 0; i < 472; i++) {
+      if (songsListRaw[i] == undefined) {
+        continue
+      } else {
+        this.songsList.push(songsListRaw[i].musicTitle[0])
+      }
+    }
+    /*
+    axios.get('https://bestdori.com/api/songs/all.5.json').then(res => {
+      for (let i = 0; i < 472; i++) {
+        const raw = res.data[i]
+        if (raw == undefined) {
+          continue
+        } else {
+          _this.songsList.push(raw.musicTitle[0])
+        }
+      }
+    })*/
+  }
+}
+</script>
+
+<style scoped>
+</style>
